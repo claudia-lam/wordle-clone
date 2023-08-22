ff37ed3129d090dcbf6b3b0b303836cf6df398eb
Claudia Lam <claudiaslam@gmail.com>
2023-08-21 23:17:55 -0700

    add form value state

--- src/components/GuessForm/GuessForm.js
+++ src/components/GuessForm/GuessForm.js
@@ -1,7 +1,32 @@
 import React from "react";
 
+/** Form for submitting guesses
+ *
+ * Props:
+ * - none
+ *
+ * State:
+ * - none
+ *
+ * Game -> GuessForm
+ */
+
 function GuessForm() {
-  return <div />;
+
+  const [formValue, setFormValue] = React.useState('');
+
+  return (
+    <form
+      class="guess-input-wrapper"
+    >
+      <label for="guess-input">Enter guess:</label>
+      <input
+        id="guess-input"
+        type="text"
+        value={formValue}
+      />
+    </form>
+  );
 }
 
 export default GuessForm;
