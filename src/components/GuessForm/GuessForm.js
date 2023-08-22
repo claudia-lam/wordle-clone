885cb7539d9ba689295740f2f688b50d75f222b3
Claudia Lam <claudiaslam@gmail.com>
2023-08-22 00:10:34 -0700

    add client side validation

--- src/components/GuessForm/GuessForm.js
+++ src/components/GuessForm/GuessForm.js
@@ -13,17 +13,29 @@ import React from "react";
 
 function GuessForm() {
 
-  const [formValue, setFormValue] = React.useState('');
+  const formInitialVal = '';
+  const [formValue, setFormValue] = React.useState(formInitialVal);
+  console.log(formValue, 'formValue');
 
   return (
     <form
-      class="guess-input-wrapper"
+      className="guess-input-wrapper"
+      onSubmit={(event) => {
+        event.preventDefault();
+        console.log("the word: ", formValue);
+        setFormValue(formInitialVal);
+      }}
     >
-      <label for="guess-input">Enter guess:</label>
+      <label htmlFor="guess-input">Enter guess:</label>
       <input
         id="guess-input"
         type="text"
+        pattern="\w{5,5}"
         value={formValue}
+        title="please enter a 5 letter word"
+        onChange={(event) => (
+          setFormValue(event.target.value.toUpperCase())
+        )}
       />
     </form>
   );
